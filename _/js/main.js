d0ce4b9052d3b1266f7b549b245a07d0de7713e4
Pim Vermeyden <pvermeyden@quarry.com>
2012-09-13 13:34:02 -0400

    allow for deleting

--- _/js/main.js
+++ _/js/main.js
@@ -1,12 +1,12 @@
 $(function() {
 	// Oh yeah
-	$('body').on('keypress', function(e) {
+	$('body').on('keydown', function(e) {
 		// Stop normal behaviour for now
 		e.preventDefault();
 		var   key		= e.keyCode
 			, letter	= [  'A','B','C','D','E','F','G','H','I','J','K','L','M'
 							,'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'  ]
-			, input		= $('.guess-word input[value=]').first();
+			, input		= $('.guess-word input[value=]');
 		
 		// Check if this is a key we want to display in the box
 		if ( 64 < key && key < 91 ) {
@@ -15,7 +15,15 @@ $(function() {
 		}
 		if ( 96 < key && key < 123 ) {
 			// letter - array the value in:
-			input.val(letter[key-97]).focus();
+			input.first().val(letter[key-97]).next().focus();
+		}
+		if ( 8 === key ) {
+			// backspace
+			input.first().prev().val('').focus();
+			if ( ! input.length ) {
+				// full boxes require clearing the last one:
+				$('.guess-word input:last-child').val('').focus();
+			}
 		}
 	});
 });
